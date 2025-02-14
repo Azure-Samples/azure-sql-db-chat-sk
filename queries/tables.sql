bd17e1712d41b5820a6f77c0e65790ca17b64229
Davide Mauri <damauri@microsoft.com>
2025-02-14 15:06:23 -0800

    added injection code

--- queries/tables.sql
+++ queries/tables.sql
@@ -2,7 +2,7 @@ select * from [dbo].[customers]
 
 select * from [dbo].[policies]
 
-select * from [dbo].[claims]
+select * from [dbo].[claims] --where customer_id = 920411 or 1=1
 
 select * from [dbo].[communication_history]
 
